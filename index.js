c51540462408c6db7dc2013ce6bda91f27f64bc2
aketsu <66210069+xiaoguihenhuai@users.noreply.github.com>
2025-06-25 16:37:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 const runChecker = async () => {
   const browser = await puppeteer.launch({ 
     headless: true,
-    executablePath: '/usr/bin/chromium', // インストールパスを確認
+    executablePath: '/opt/homebrew/bin/chromium', // インストールパスを確認
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
